8a5a467413e8c88e8974bf4cee53ad3a08399bf3
razzzzzr <alex.goryashkiev@gmail.com>
2023-04-02 23:54:04 +0300

    add endpoints /task/{id}/(complete,incomplete,delegate)

--- task_tracker/manager/permissions.py
+++ task_tracker/manager/permissions.py
@@ -10,3 +10,19 @@ class IsOwnerOrAssignedOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user == obj.owner or request.user == obj.assigned_to
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission for checking object-level permissions.
+    Allows read-only operations for all users and write operations for the task owner or the assigned user.
+    """
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user == obj.owner
+
+
+class IsOwnerOrAssignedTo(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.owner or request.user == obj.assigned_to
--- task_tracker/manager/views.py
+++ task_tracker/manager/views.py
@@ -1,16 +1,34 @@
-from django.db.models import Q
-from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from manager.models import Task
 from manager.serializers import TaskSerializer
-from manager.permissions import IsOwnerOrAssignedOrReadOnly
-
-
+from manager.permissions import IsOwnerOrReadOnly, IsOwnerOrAssignedTo
+from manager.services import user_exist
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = (IsOwnerOrAssignedOrReadOnly, )
+    permission_classes = (IsOwnerOrReadOnly, )
 
-    # def get_queryset(self):
-    #     user = self.request.user
-    #     return Task.objects.filter(Q(owner=user) | Q(assigned_to=user)).order_by('id')
+    @action(detail=True, methods=['post'], permission_classes=(IsOwnerOrAssignedTo,))
+    def complete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = True
+        task.save()
+        return Response({'message': 'Task marked as completed'}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'], permission_classes=(IsOwnerOrAssignedTo,))
+    def incomplete(self, request, pk=None):
+        task = self.get_object()
+        task.completed = False
+        task.save()
+        return Response({'message': 'Task marked as not completed'}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'], permission_classes=(IsOwnerOrAssignedTo,))
+    def delegate(self, request, pk=None):
+        task = self.get_object()
+        user = user_exist(request.data['assigned_to'])
+        if user:
+            task.assigned_to = user
+            task.save()
+            return Response({'message': 'Task was delegated'}, status=status.HTTP_200_OK)
