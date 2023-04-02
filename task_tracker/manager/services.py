bb4946b0c8c493908083b1763ee66e6dc783283c
razzzzzr <alex.goryashkiev@gmail.com>
2023-04-03 00:27:08 +0300

    refractor viewsets

--- task_tracker/manager/services.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import get_object_or_404
-from accounts.models import CustomUser
-
-
-def user_exist(username):
-    user = get_object_or_404(CustomUser, username=username)
-    return user
--- task_tracker/manager/views.py
+++ task_tracker/manager/views.py
@@ -1,14 +1,18 @@
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from manager.models import Task
 from manager.serializers import TaskSerializer
 from manager.permissions import IsOwnerOrReadOnly, IsOwnerOrAssignedTo
-from manager.services import user_exist
+from accounts.models import CustomUser
+
+
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-    permission_classes = (IsOwnerOrReadOnly, )
+    permission_classes = (IsAuthenticated, IsOwnerOrReadOnly, )
 
     @action(detail=True, methods=['post'], permission_classes=(IsOwnerOrAssignedTo,))
     def complete(self, request, pk=None):
@@ -27,8 +31,11 @@ class TaskViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['post'], permission_classes=(IsOwnerOrAssignedTo,))
     def delegate(self, request, pk=None):
         task = self.get_object()
-        user = user_exist(request.data['assigned_to'])
-        if user:
+        assigned_to = request.data.get('assigned_to')
+        if assigned_to:
+            user = get_object_or_404(CustomUser, username=assigned_to)
             task.assigned_to = user
             task.save()
             return Response({'message': 'Task was delegated'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'assigned_to field is required'}, status=status.HTTP_400_BAD_REQUEST)
